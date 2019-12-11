2dd4914dd3bc55a1d57b587a542cd4169fa47725
Alex Larcheveque <larchevequealex@gmail.com>
2019-12-10 16:23:14 -0800

    Figured out how to use Fibonacci sequence with memoization. Learned some new stuff regarding closures and lexical scoping

--- CodingUdemyCourse/exercises/fib/index.js
+++ CodingUdemyCourse/exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Solution 3: Solving Fibbonanci Recursively w/ Memoization
+function memoizator(fn, memoMap = {}) {
+  console.log("inside memoizator");
+  return n => (memoMap[n] ? memoMap[n] : (memoMap[n] = fn(n)));
+}
+
+function slowFib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoizator(slowFib);
 
 module.exports = fib;
+
+// //Solution: Iterate through a for-loop, pre-determine the 0th and 1st numbers
+// function fib(n) {
+//   let fib = [];
+//   fib[0] = 0;
+//   fib[1] = 1;
+
+//   for (let i = 2; i <= n; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2];
+//   }
+//   return fib[n];
+// }
+
+// //Solution 2: Solving Fibbonanci Recursively
+// function fib(n) {
+//   if (n < 2) return n;
+//   return fib(n - 1) + fib(n - 2);
+// }
