0e9bf364d3970979968484c473dd94eb221682bb
Alex Larcheveque <larchevequealex@gmail.com>
2020-01-17 15:20:32 -0800

    Inverse Binary Tree

---
+++ Trees/226-InvertBinaryTree.js
@@ -0,0 +1,23 @@
+//Run Time --> O(n)
+//Space Complexity --> O(n)
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (root) {
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+  }
+
+  return root;
+};
