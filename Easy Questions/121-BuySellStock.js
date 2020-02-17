c78b3e52afdaa2eca288cb490889118f4fac000e
alexlarcheveque <larchevequealex@gmail.com>
2020-02-16 23:56:11 -0600

    Revised problem with better algorithm (non-brute force)

--- Easy Questions/121-BuySellStock.js
+++ Easy Questions/121-BuySellStock.js
@@ -1,3 +1,4 @@
+//Brute Force Method
 //Runtime --> O(n^2)
 //Space --> O(1)
 
@@ -17,4 +18,29 @@ var maxProfit = function (prices) {
     }
 
     return max;
+};
+
+//One-Pass Method
+//Runtime --> O(n)
+//Space --> O(1)
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (minPrice > prices[i]) {
+            minPrice = prices[i]
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+
 };
